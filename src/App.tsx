866fdbf7352f51029e5bc0be1470238489b5d6cc
André Canton <andrecanton18@gmail.com>
2022-07-29 10:10:31 -0300

    feat: add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Catalog from './features/Catalog';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Catalog from "./features/Catalog";
+import Layout from "./features/Layout";
+import './App.scss'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Catalog />}/>
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Catalog />} />
+      </Routes>
+    </Layout>
   );
 }
 
