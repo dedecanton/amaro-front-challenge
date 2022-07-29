634c54b9f858a6b4538bfe27a1b531c890faaa2e
André Canton <andrecanton18@gmail.com>
2022-07-29 10:09:30 -0300

    feat: footer

---
+++ src/features/Layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Aplicação desenvolvida por <a href="https://linkedin.com/in/andre-canton" target={'_blank'}>André Canton</a></p>
+    </footer>
+  )
+}
+
+export default Footer
