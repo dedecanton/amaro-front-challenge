2114c51a5eb38f2f5d2ebe20d2e8144479962ff4
André Canton <andrecanton18@gmail.com>
2022-07-29 10:08:29 -0300

    feat: header scrolled effect

--- src/features/Layout/Header/Header.tsx
+++ src/features/Layout/Header/Header.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AmaroLogo from "../../../assets/AmaroLogo";
 import CartIcon from "./CartIcon";
 import "./Header.scss";
 
 const Header = () => {
-
   const navigate = useNavigate()
+  const [isScrolled, setIsScrolled] = useState<boolean>(false)
+  
+  const stickyHandler = () => {
+    window.scrollY > 30 ? setIsScrolled(true) : setIsScrolled(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', stickyHandler)
+  }, [])
 
   const handleLogoClick = () => {
     navigate('/')
@@ -17,7 +25,7 @@ const Header = () => {
 
   return (
     <header>
-      <div className="header__container">
+      <div className={"header__container " + (isScrolled ? 'header__container-scrolled' : '')}>
         <div className="header__image" onClick={handleLogoClick}>
           <AmaroLogo />
         </div>
