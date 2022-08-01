27b7dcf6d43b5135f4a0ea5a8a5f8a017a938458
André Canton <andrecanton18@gmail.com>
2022-08-01 11:08:21 -0300

    feat: show totalItems in icon

--- src/features/Layout/Header/CartIcon/CartIcon.tsx
+++ src/features/Layout/Header/CartIcon/CartIcon.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import './CartIcon.scss'
 import { BsBag } from "react-icons/bs";
+import { useAppSelector } from '../../../../store/hooks';
 
 type CartIconProps = {
     onClick: () => void
 }
 
 const CartIcon = ({onClick}:CartIconProps) => {
+  
+  const totalItems = useAppSelector(state => state.cartListItem).reduce((acc, item) => {
+    return acc + item.quantity
+  }, 0)
+
+
   return (
     <div className='cart-icon__container' onClick={onClick}>
         <BsBag />
-        <span className='cart-icon__quantity'>0</span>
+        <span className='cart-icon__quantity'>{totalItems}</span>
     </div>
   )
 }
