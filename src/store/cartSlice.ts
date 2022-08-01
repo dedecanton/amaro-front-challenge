5e932a97e1e9c350b72291f4cbd5f257f6e8fa34
André Canton <andrecanton18@gmail.com>
2022-08-01 11:05:33 -0300

    feat: use helper functions, add increment, decrement and delete actions

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CartItem, CartStore, Product } from "../types/index.types";
+import createNewCartItem from "../utils/createNewCartItem";
+import findCartItem from "../utils/findCarItem";
 
 const initialState: CartStore = {
   cartListItem: [],
@@ -10,31 +12,62 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItemToCart: (state, action: PayloadAction<{product: Product, size: string}>) => {
-        const product = action.payload.product
-        const id = product.code_color
-        const size = action.payload.size
+    addItemToCart: (
+      state,
+      action: PayloadAction<{ product: Product; size: string }>
+    ) => {
+      const { product, size } = action.payload;
+      const id = product.code_color + size;
 
-        const existingItem = state.cartListItem.find(item => item.id === id && item.size === size)
+      const existingItem = findCartItem(id, size, state.cartListItem);
 
-        if(existingItem){
-            existingItem.quantity++;
-            existingItem.totalPrice += existingItem.price
-        }else{
-            const price = parseFloat(product.actual_price.split(' ')[1].replace(',','.'))
-            state.cartListItem.push({
-                image: product.image,
-                name: product.name,
-                id: product.code_color,
-                price,
-                quantity: 1,
-                size,
-                totalPrice: price,
-            })
-        }
-        
-        
-    }
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += existingItem.price;
+      } else {
+        const newProduct = createNewCartItem(product, size);
+        state.cartListItem.push(newProduct);
+      }
+    },
+    incrementItem: (
+      state,
+      action: PayloadAction<{ id: string; size: string }>
+    ) => {
+      const { id, size } = action.payload;
+      const existingItem = findCartItem(id, size, state.cartListItem);
+
+      if (!existingItem) throw new Error("Product not found");
+
+      existingItem.quantity++;
+      existingItem.totalPrice += existingItem.price;
+    },
+    decrementItem: (
+      state,
+      action: PayloadAction<{ id: string; size: string }>
+    ) => {
+      const { id, size } = action.payload;
+      const existingItem = findCartItem(id, size, state.cartListItem);
+
+      if (!existingItem) throw new Error("Product not found");
+
+      if (existingItem.quantity === 1) {
+        state.cartListItem = state.cartListItem.filter(
+          (item) => item.id !== id || item.size !== size
+        );
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
+    deleteItem: (
+      state,
+      action: PayloadAction<{ id: string; size: string }>
+    ) => {
+      const { id, size } = action.payload;
+      state.cartListItem = state.cartListItem.filter(
+        (item) => item.id !== id || item.size !== size
+      );
+    },
   },
 });
 
