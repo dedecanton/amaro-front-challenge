818425a3c05e32c924b3102b2e974a8c507a1476
André Canton <andrecanton18@gmail.com>
2022-08-01 12:07:22 -0300

    feat: persist state with redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store, persistor } from './store';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,9 +13,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <BrowserRouter>
       <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,21 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { cartReducer } from './cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 
 export const store = configureStore({
-  reducer: cartReducer,
-});
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+})
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
